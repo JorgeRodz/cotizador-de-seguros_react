33a23fae95f1ecd73912ede51661759fc0513d0c
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2021-10-22 02:03:44 -0500

    Add a helper to do some calculation for the Form component

---
+++ src/helper.js
@@ -0,0 +1,28 @@
+// Obtiene la diferencia de años
+export const obtenerDiffYear = (year) => {
+  return new Date().getFullYear() - year;
+};
+
+// Calcula el total a pegar segun la marca
+export const calcularMarca = (marca) => {
+  let incremento;
+
+  /* prettier-ignore */
+  switch (marca) {
+    case "Europeo":
+      incremento = 1.30;
+      break;
+    case "Americano":
+      incremento = 1.15;
+      break;
+    default:
+      incremento = 1.05;
+  }
+
+  return incremento;
+};
+
+// Calcula el tipo de seguro
+export const obtenerPlan = (plan) => {
+  return plan === "basico" ? 1.2 : 1.5;
+};
